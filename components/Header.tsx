ebf6f4f1239714d138a05aae33f3986b3adcc5e2
Ryan <ryannichpurba44@gmail.com>
2026-08-25 12:37:02 +0700

    update: implement header navigation links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
-import Link from 'next/link';
-import Image from 'next/image';
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header>
       <div className="main-container inner">
@@ -13,9 +19,30 @@ const Header = () => {
             height={50}
           />
         </Link>
+
+        <nav>
+          <Link
+            href="/"
+            className={cn("nav-link", {
+              "is-active": pathname == "/",
+              "is-home": true
+            })}
+          >
+            Home
+          </Link>
+
+          <p>Search Modal</p>
+
+          <Link
+            href="/coins"
+            className={cn("nav-link", { "is-active": pathname == "/coins" })}
+          >
+            All Coins
+          </Link>
+        </nav>
       </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
